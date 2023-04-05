cf871dd79bfbee73cf244813fcfdbc7be2c4fea9
ThewBear <26767607+ThewBear@users.noreply.github.com>
2023-04-05 18:17:46 +0700

    Use edge middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import type { VercelRequest, VercelResponse  } from "@vercel/node";
-import * as Amplitude from "@amplitude/node";
 import { RedirectConfig } from "./cli";
 
 export interface RedirectApiURLConfig extends Omit<RedirectConfig, "regex"> {
@@ -11,44 +9,65 @@ interface RedirectApiConfig {
   NotFoundPage: string;
 }
 
-const amplitude = process.env.Amplitude
-  ? Amplitude.init(process.env.Amplitude)
-  : null;
+interface AmplitudeEvent {
+  event_type: string;
+  event_properties: Object;
+}
 
-function sendAmplitude(
-  req: VercelRequest,
-  event: Amplitude.Event
-): Promise<Amplitude.Response | void> {
-  if (amplitude) {
-    const requestId = String(req.headers["x-vercel-id"]);
-    amplitude.logEvent({
-      ip: String(req.headers["x-real-ip"]),
-      user_id: requestId,
-      user_properties: {
-        regions: requestId.substring(0, requestId.lastIndexOf("::")),
-        podId: requestId.substring(
-          requestId.lastIndexOf("::") + 2,
-          requestId.indexOf("-")
-        ),
-      },
-      ...event,
-    });
+function sendAmplitude(request: Request, event: AmplitudeEvent): Promise<void> {
+  if (!process.env.Amplitude) return Promise.resolve();
+  const requestId = String(request.headers.get("x-vercel-id"));
+  const realIp = String(request.headers.get("x-real-ip"));
+  const host = String(request.headers.get("host"));
+  const deploymentUrl = String(request.headers.get("x-vercel-deployment-url"));
 
-    return amplitude.flush();
-  }
-  return Promise.resolve();
+  return fetch("https://api2.amplitude.com/2/httpapi", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "*/*",
+    },
+    body: JSON.stringify({
+      api_key: process.env.Amplitude,
+      events: [
+        {
+          user_id: requestId,
+          user_properties: {
+            regions: requestId.substring(0, requestId.lastIndexOf("::")),
+            podId: requestId.substring(
+              requestId.lastIndexOf("::") + 2,
+              requestId.indexOf("-")
+            ),
+            host: host,
+            deploymentUrl: deploymentUrl,
+          },
+          ip: realIp,
+          event_type: event.event_type,
+          event_properties: event.event_properties,
+        },
+      ],
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
 }
 
 export default class RedirectApi {
   constructor(readonly config: RedirectApiConfig) {}
 
-  handler(req: VercelRequest, res: VercelResponse ) {
-    if (!req.query.path) {
-      return res.status(404).send(this.config.NotFoundPage);
-    }
+  handler(request: Request) {
+    const requestUrl = new URL(request.url);
 
-    // Add starting "/" and remove trailing "/"
-    const requestPath = "/" + String(req.query.path).replace(/\/$/, "");
+    // Remove trailing "/"
+    const requestPath = String(requestUrl.pathname).replace(/\/$/, "");
+
+    if (requestPath === "") {
+      return sendAmplitude(request, {
+        event_type: "index",
+        event_properties: { source: requestPath },
+      });
+    }
 
     for (const url of this.config.urls) {
       const pathMatch = requestPath.match(url.regex);
@@ -56,7 +75,7 @@ export default class RedirectApi {
         const variables = pathMatch.groups || {};
         // Must have query as specified in config
         const queryMatch = Object.keys(url.query || {}).every((key) => {
-          const requestValue = String(req.query[key] || "");
+          const requestValue = String(requestUrl.searchParams.get(key) || "");
           if (!url.query?.[key].startsWith(":")) {
             // Exact string match
             return url.query?.[key] === requestValue;
@@ -64,9 +83,8 @@ export default class RedirectApi {
             // Variable query
             if (url.query[key].endsWith("?") || requestValue) {
               // Have query or optional blank
-              variables[
-                url.query[key].replace("?", "").substring(1)
-              ] = requestValue;
+              variables[url.query[key].replace("?", "").substring(1)] =
+                requestValue;
               return true;
             } else {
               // Doesn't have required query
@@ -80,17 +98,25 @@ export default class RedirectApi {
           for (const key in variables) {
             destination = destination.replace(`:${key}`, variables[key] || "");
           }
-          return sendAmplitude(req, {
+          return sendAmplitude(request, {
             event_type: "go",
             event_properties: { source: requestPath, destination },
-          }).then(() => res.redirect(url.status || 307, destination));
+          }).then(() => Response.redirect(destination, url.status || 307));
         }
       }
     }
 
-    return sendAmplitude(req, {
+    return sendAmplitude(request, {
       event_type: "Not Found",
       event_properties: { source: requestPath },
-    }).then(() => res.status(404).send(this.config.NotFoundPage));
+    }).then(
+      () =>
+        new Response(this.config.NotFoundPage, {
+          status: 404,
+          headers: {
+            "content-type": "text/html",
+          },
+        })
+    );
   }
 }
